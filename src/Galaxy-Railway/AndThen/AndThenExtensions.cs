f565ac67b929e1745fb57757934cdac0c89f8e59
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-06-09 22:20:31 +0200

    new extensions adds

---
+++ src/Galaxy-Railway/AndThen/AndThenExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Galaxy_Railway.AndThen
+{
+    public static class AndThenExtensions
+    {
+        public static TResult AndThen<T, TResult>(this T @obj, Func<T, TResult> func) => func(@obj);
+
+        public static T AndThen<T>(this T @obj, Action<T> func)
+        {
+            func(@obj);
+            return @obj;
+        }
+
+        public static async Task<TResult> AndThenAsync<T, TResult>(this Task<T> @task,
+           Func<T, Task<TResult>> func) =>
+           await func((await task.ConfigureAwait(false))).ConfigureAwait(false);
+
+        public static async Task<TResult> AndThenAsync<T, TResult>(this Task<T> @task,
+            Func<T, TResult> func) =>
+            func((await task.ConfigureAwait(false)));
+    }
+}
---
+++ src/Galaxy-Railway/With/WithExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Galaxy_Railway.With
+{
+    public static class WithExtensions
+    {
+        public static T With<T>(this T @obj, Action<T> update)
+        {
+            update(@obj);
+            return @obj;
+        }
+
+        public static async Task<T> WithAsync<T>(this T @obj, Func<T, Task> update)
+        {
+            await update(@obj).ConfigureAwait(false);
+            return @obj;
+        }
+
+        public static async Task<T> WithAsync<T>(this Task<T> @task, Func<T, Task> update)
+        {
+            var result = await @task.ConfigureAwait(false);
+
+            await update(result).ConfigureAwait(false);
+
+            return result;
+        }
+
+        public static async Task<T> WithAsync<T>(this Task<T> @task, Action<T> update)
+        {
+            var result = await @task.ConfigureAwait(false);
+
+            update(result);
+
+            return result;
+        }
+    }
+}

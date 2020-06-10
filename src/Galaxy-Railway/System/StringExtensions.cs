8c67f9594a44d82bad5c08efeee0b6c06080b82a
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-06-10 14:24:15 +0200

    isnull method adds

--- src/Galaxy-Railway/System/StringExtensions.cs
+++ src/Galaxy-Railway/System/StringExtensions.cs
@@ -2,10 +2,8 @@
 {
     public static class StringExtensions
     {
-        public static bool IsNullOrEmpty(this string @this) =>
-             string.IsNullOrEmpty(@this);
-         
-        public static bool IsNullOrWhiteSpace(this string @this) =>
-            string.IsNullOrWhiteSpace(@this); 
+        public static bool IsNull(this string @this) => @this == null;
+        public static bool IsNullOrEmpty(this string @this) => string.IsNullOrEmpty(@this); 
+        public static bool IsNullOrWhiteSpace(this string @this) => string.IsNullOrWhiteSpace(@this);
     }
 }

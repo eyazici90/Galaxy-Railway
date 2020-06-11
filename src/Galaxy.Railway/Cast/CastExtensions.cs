c222b8c84ba2b912c602a433399538bff55c5618
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-06-11 23:01:43 +0200

    if&cast extensions adds

---
+++ src/Galaxy.Railway/Cast/CastExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Galaxy.Railway
+{
+    public static class CastExtensions
+    {
+        public static T As<T>(this object @this)
+              where T : class =>
+             (T)@this;
+
+
+        public static T To<T>(this object @this)
+            where T : struct =>
+            (T)Convert.ChangeType(@this, typeof(T), CultureInfo.InvariantCulture);
+
+    }
+}
---
+++ src/Galaxy.Railway/If/IfExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Galaxy.Railway
+{
+    public static class IfExtensions
+    {
+        public static T If<T>(this T @this, bool condition, Func<T, T> func)
+        {
+            if (condition)
+                return func(@this);
+
+            return @this;
+        }
+
+        public static T If<T>(this T @this, bool condition, Action<T> action)
+        {
+            if (condition)
+                action(@this);
+
+            return @this;
+        }
+
+        public static async Task<T> IfAsync<T>(this T @this, bool condition, Func<T, Task> func)
+        {
+            if (condition)
+                await func(@this).ConfigureAwait(false);
+
+            return @this;
+        }
+
+        public static async Task<T> IfAsync<T>(this Task<T> @this, bool condition, Func<T, Task> func)
+        {
+            var result = await @this.ConfigureAwait(false);
+
+            if (condition)
+                await func(result).ConfigureAwait(false);
+
+            return result;
+        }
+
+        public static async Task<T> IfAsync<T>(this Task<T> @this, bool condition, Action<T> action)
+        {
+            var result = await @this.ConfigureAwait(false);
+
+            if (condition)
+                action(result);
+
+            return result;
+        }
+    }
+}

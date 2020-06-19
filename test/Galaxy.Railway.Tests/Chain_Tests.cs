7f48c4527c924e7af811f75e0967c81c32a89dc6
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-06-19 12:17:53 +0200

    chain tests adds

---
+++ test/Galaxy.Railway.Tests/Chain_Tests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Galaxy.Railway;
+using System;
+using Xunit;
+
+namespace Galaxy_Railway.Tests
+{
+    public class Chain_Tests
+    {
+        [Fact]
+        public void Should_be_chained()
+        {
+            var city = "Amsterdam";
+
+            var result = city.ToOptional()
+                .ThrowsIf(optional => !optional.HasValue, new ArgumentNullException())
+                .Map(result => result.Value)
+                .AndThen(c => c.ToLower());
+
+            result.Should().Be(city.ToLower());
+        }
+
+        [Fact]
+        public void Should_throw()
+        {
+            string city = null;
+
+            Action act = () => city.ToOptional()
+                .ThrowsIf(optional => !optional.HasValue, new ArgumentNullException())
+                .Map(result => result.Value)
+                .AndThen(c => c.ToLower());
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ test/Galaxy.Railway.Tests/Optional_Tests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Galaxy.Railway;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Galaxy_Railway.Tests
+{
+    public class Optional_Tests
+    {
+        [Fact]
+        public void Should_be_optional_has_value()
+        {
+            var result = string.Empty.ToOptional();
+
+            result.Should().BeOfType<Optional<string>>();
+            result.HasValue.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_be_optional_has_not_value()
+        {
+            string fake = null;
+
+            var result = fake.ToOptional();
+
+            result.Should().BeOfType<Optional<string>>();
+            result.HasValue.Should().BeFalse();
+        }
+        [Fact]
+        public async Task Should_be_optional_async()
+        {
+            var result = await Task.FromResult(string.Empty).ToOptionalAsync();
+
+            result.Should().BeOfType<Optional<string>>();
+            result.HasValue.Should().BeTrue();
+        }
+    }
+}
